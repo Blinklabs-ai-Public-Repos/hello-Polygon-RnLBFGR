a10e50d5ee46daa7226ceaf123ed5715ce28f636
blinklabs-admin <mike@blinklabs.ai>
2024-07-03 09:33:11 +0800

    Update the contract with new feature of Token Burning: Implement a mechanism to periodically burn a percentage of tokens from each transfer, creating deflationary tokenomics.

--- packages/hardhat/contracts/JKToken.sol
+++ packages/hardhat/contracts/JKToken.sol
@@ -5,9 +5,12 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract JKToken is ERC20, Ownable {
-    uint256 private constant INITIAL_SUPPLY = 1000000 * 10**18; // 1 million tokens
+    uint256 private immutable INITIAL_SUPPLY;
+    uint256 public constant BURN_RATE = 1; // 1% burn rate
+    uint256 public constant BURN_DENOMINATOR = 100;
 
-    constructor(string memory name_, string memory symbol_) ERC20(name_, symbol_) {
+    constructor(string memory name_, string memory symbol_, uint256 initialSupply_) ERC20(name_, symbol_) {
+        INITIAL_SUPPLY = initialSupply_ * 10**decimals();
         _mint(msg.sender, INITIAL_SUPPLY);
     }
 
@@ -17,5 +20,13 @@ contract JKToken is ERC20, Ownable {
 
     function burn(uint256 amount) public {
         _burn(_msgSender(), amount);
+    }
+
+    function _transfer(address sender, address recipient, uint256 amount) internal override {
+        uint256 burnAmount = (amount * BURN_RATE) / BURN_DENOMINATOR;
+        uint256 transferAmount = amount - burnAmount;
+
+        super._transfer(sender, recipient, transferAmount);
+        _burn(sender, burnAmount);
     }
 }
